8940bc8ea3b183baa5370d085a599e0e1c860c9b
Benson Li <zl791@nyu.edu>
2020-10-01 15:08:19 -0400

    fix proxy

--- orange_proxy/__init__.py
+++ orange_proxy/__init__.py
@@ -10,55 +10,32 @@ CHUNK_SIZE = 1024
 LOG = logging.getLogger("app.py")
 
 
-def merge_two_dicts(x, y):
-    z = x.copy()   # start with x's keys and values
-    z.update(y)    # modifies z with y's keys and values & returns None
-    return z
-
-
-def set_header():
-    headers = {
-        'X-Forwarded-For': "113.198.69.0",
-        'X-Msisdn': "+14125006666"
-    }
-    return headers
-
-
 @app.route('/')
 def root():
-    query = urlparse(request.full_path).query
-    query_components = dict(parse_qsl(query))
-    next = query_components.get('n', None)
-    LOG.debug("next param: %s", next)
+    next = get_next_url()
     if (next is not None):
-        enriched_headers = merge_two_dicts(dict(request.headers), set_header())
+        enriched_headers = get_enrich_headers()
+        enriched_headers.pop('Host')
         r = make_request(
             next,
             request.method,
             enriched_headers,
-            request.form
         )
-        LOG.critical("response:  %s", r)
-        LOG.critical("enriched headers: %s", enriched_headers)
-
-        if (r.status_code == 302):
-            LOG.critical("redirect")
-            redirect(r.headers['Location'])
 
         def generate():
             for chunk in r.raw.stream(decode_content=False):
                 yield chunk
-        out = Response(generate(), headers=dict(r.headers))
-        out.status_code = r.status_code
-        LOG.critical("out: %s", out)
-        return out
+        response = Response(generate(), headers=dict(r.headers))
+        response.status_code = r.status_code
+        LOG.critical("response: %s\n", response)
+
+        return response
     return "Missing next param url 'n'"
 
 
-def make_request(url, method, headers={}, data=None):
-    # Fetch the URL, and stream it back
-    LOG.debug("make_request")
-    LOG.debug("Sending %s %s with headers: %s and data %s", method, url, headers, data)
+def make_request(url, method, headers={}):
+    LOG.debug("making request...")
+    LOG.debug("Sending %s %s with headers: %s\n", method, url, headers)
     resp = requests.request(
         method,
         url,
@@ -66,10 +43,27 @@ def make_request(url, method, headers={}, data=None):
         stream=True,
         headers=headers,
         allow_redirects=False,
-        data=data
     )
     return resp
 
+
+def get_enrich_headers():
+    inject = {
+        'X-Forwarded-For': "113.198.69.0",
+        'X-Msisdn': "+14125006666"
+    }
+    enriched = dict(request.headers).copy()
+    enriched.update(inject)
+    LOG.debug("enriched headers: %s\n", enriched)
+    return enriched
+
+
+def get_next_url():
+    query = urlparse(request.full_path).query
+    query_components = dict(parse_qsl(query))
+    next = query_components.get('n', None)
+    LOG.debug("next param: %s", next)
+    return next
 
 if __name__ == "__main__":
     app.run()
